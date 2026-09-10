c15fbcea2b13cc7f16428543da4e08262bbf5579
Bhavesh Laku <bhaveshlakupbl@gmail.com>
2026-09-10 09:32:37 +0530

    add required repositories

---
+++ src/main/java/com/bhavesh/roovia/repository/GuestRepository.java
@@ -0,0 +1,11 @@
+//package com.bhavesh.roovia.repository;
+//
+//import com.bhavesh.roovia.entity.Guest;
+//import com.bhavesh.roovia.entity.User;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import java.util.List;
+//
+//public interface GuestRepository extends JpaRepository<Guest, Long> {
+//    List<Guest> findByUser(User user);
+//}
